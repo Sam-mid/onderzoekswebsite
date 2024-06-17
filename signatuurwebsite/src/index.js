ffb0fa5f8175e9021d3977e218f2b1d268c2c9a7
Sam Middendorp <sammiddendorp2002@gmail.com>
2024-06-17 18:41:48 +0200

    unversion files

--- signatuurwebsite/src/index.js
+++ signatuurwebsite/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
